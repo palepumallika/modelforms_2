73c22ee4a0e8397730cabd7ceec47b7b056fdac9
palepumallika <palepumallika@gmail.com>
2022-12-26 19:34:55 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -9,3 +9,8 @@ class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
+         #fields=['topic_name','name']
+        #exclude=['name']
+        help_texts={'topic_name':'should not be integers','name':'only Alphabets'}
+        labels={'topic_name':'TN','name':'N'}
+        widgets={'url':forms.PasswordInput,'name':forms.Textarea}
